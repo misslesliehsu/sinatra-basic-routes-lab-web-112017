23c29578648168b8f19982f668528e82bc2bd152
Leslie Hsu <miss.leslie.hsu@gmail.com>
2017-12-12 23:16:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/hometown' do
+    "My hometown is San Diego"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Yeah!' by Usher."
+  end
+
+  get '/name' do
+    "My name is Leslie"
+  end
+
 end
